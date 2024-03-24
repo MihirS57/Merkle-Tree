6b4db0ca6a096400726dfde5afdc700dc58cafa2
MihirS57 <shahmihir507@gmail.com>
2024-03-23 18:19:37 -0700

    Changes made to include both hash and data for each node in the merkle tree

--- buildmtree.py
+++ buildmtree.py
@@ -3,10 +3,11 @@ import json
 import sys
 
 class MerkleTreeObj:
-    def __init__(self,data):
+    def __init__(self,data,hash_value):
         self.left = None
         self.right = None
         self.data = data
+        self.hash_value = hash_value
 
 def adjustDataItems(size,value_list):
     i = 2
@@ -29,14 +30,15 @@ def buildMerkleTree(value_list):
     num_nodes = len(value_list)
     nodes_list = []
     for ind in range(num_nodes):
-        nodes_list.append(MerkleTreeObj(data=value_list[ind]))
+        nodes_list.append(MerkleTreeObj(value_list[ind],
+        sha256(value_list[ind].encode('utf-8')).hexdigest()))
     i = 0
     while i < num_nodes:
         if i+1 < num_nodes:
             node_left = nodes_list[i]
             node_right = nodes_list[i+1]
-            concat_val = node_left.data+node_right.data
-            parent_hash = MerkleTreeObj(sha256(concat_val.encode('utf-8')).hexdigest())
+            concat_val = node_left.hash_value+node_right.hash_value
+            parent_hash = MerkleTreeObj(concat_val,sha256(concat_val.encode('utf-8')).hexdigest())
             nodes_list.append(parent_hash)
             parent_hash.left = node_left
             parent_hash.right = node_right
@@ -53,17 +55,27 @@ def bfs(node,q):
         q.append(node.left)
         q.append(node.right)
         tree_dic = {
-            "parent": node.data,
-            "left_child": None if node.left == None else node.left.data,
-            "right_child": None if node.right == None else node.right.data
+            "parent": {
+                "data": node.data,
+                "hash": node.hash_value
+            },
+            "left_child": {
+                "data": None if node.left == None else node.left.data,
+                "hash": None if node.left == None else node.left.hash_value,
+            },
+            "right_child": {
+                "data": None if node.left == None else node.right.data,
+                "hash": None if node.left == None else node.right.hash_value,
+            }
         }
         json_list.append(tree_dic)
     while len(q) > 0:
         bfs(q.pop(0),q)
 
     
-print('Enter words separated using commas (for eg: your input can be <mihir,shah,viterbi,csci> without the "<" or ">" ): ')
+print('Enter words separated using commas (for eg: your input can be /[mihir,shah,viterbi,csci/]> ): ')
 value_input = sys.argv[1]
+value_input = value_input[1:len(value_input)-1]
 value_list = value_input.split(",")
 print(f'Original Data: {value_list}')
 value_list = adjustDataItems(len(value_list),value_list)
--- checkinclusion.py
+++ checkinclusion.py
@@ -3,10 +3,11 @@ import json
 import sys
 
 class MerkleTreeObj:
-    def __init__(self,data):
+    def __init__(self,data,hash_value):
         self.left = None
         self.right = None
         self.data = data
+        self.hash_value = hash_value
 
 def buildTreeFromFile():
     map = {}
@@ -15,15 +16,15 @@ def buildTreeFromFile():
     root = None
     for item in tree_data:
         parent = item["parent"]
-        map[parent] = MerkleTreeObj(parent)
+        map[parent["data"]] = MerkleTreeObj(parent["data"],parent["hash"])
     for item in tree_data:
         parent = item["parent"]
         if root is None:
-            root = map[parent]
+            root = map[parent["data"]]
         left_val = item["left_child"]
         right_val = item["right_child"]
-        map[parent].left = map[left_val] if left_val != None else None
-        map[parent].right = map[right_val] if right_val != None else None
+        map[parent["data"]].left = map[left_val["data"]] if left_val["data"] != None else None
+        map[parent["data"]].right = map[right_val["data"]] if right_val["data"] != None else None
     return root
 
 def checkInclusion(temp_root,searchItem):
@@ -38,29 +39,29 @@ def checkInclusion(temp_root,searchItem):
     respRight = checkInclusion(temp_root.right,searchItem)
 
     if respLeft == False and respRight:
-        if temp_root.left.data not in hash_set:
-            hash_set.add(temp_root.left.data)
-            hash_list.append(temp_root.left.data)
-            proveInclusion = sha256((temp_root.left.data+proveInclusion).encode('utf-8')).hexdigest()
+        if temp_root.left.hash_value not in hash_set:
+            hash_set.add(temp_root.left.hash_value)
+            hash_list.append(temp_root.left.hash_value)
+            proveInclusion = sha256((temp_root.left.hash_value+proveInclusion).encode('utf-8')).hexdigest()
     elif respLeft and respRight == False:
-        if temp_root.right.data not in hash_set:
-            hash_set.add(temp_root.right.data)
-            hash_list.append(temp_root.right.data)
-            proveInclusion = sha256((proveInclusion+temp_root.right.data).encode('utf-8')).hexdigest()
+        if temp_root.right.hash_value not in hash_set:
+            hash_set.add(temp_root.right.hash_value)
+            hash_list.append(temp_root.right.hash_value)
+            proveInclusion = sha256((proveInclusion+temp_root.right.hash_value).encode('utf-8')).hexdigest()
     elif respLeft and respRight:
-        if temp_root.left.data not in hash_set:
-            hash_set.add(temp_root.left.data)
-            hash_list.append(temp_root.left.data)
-            proveInclusion = sha256((temp_root.left.data+proveInclusion).encode('utf-8')).hexdigest()
+        if temp_root.left.hash_value not in hash_set:
+            hash_set.add(temp_root.left.hash_value)
+            hash_list.append(temp_root.left.hash_value)
+            proveInclusion = sha256((temp_root.left.hash_value+proveInclusion).encode('utf-8')).hexdigest()
     return respLeft or respRight
 
 searchItem = sys.argv[1]
 hash_set = set()
 hash_list = []
-proveInclusion = searchItem;
+proveInclusion = sha256(searchItem.encode('utf-8')).hexdigest();
 tree_root = buildTreeFromFile()
 searchResults = checkInclusion(tree_root,searchItem)
-if searchResults and tree_root.data == proveInclusion:
+if searchResults and tree_root.hash_value == proveInclusion:
     print(f'yes {hash_list}')
 else:
     print("No")
